5c9f1c5098ab6ea4a7593f184c1ddcdffb8dfb10
Erik Primao <erikprimao@gmail.com>
2026-05-20 23:16:27 -0300

    salvando projeto

---
+++ admin/includes/assets.php
@@ -0,0 +1,13 @@
+<meta name="viewport" content="width=device-width, user-scalable=no">
+
+<meta name="title" content="FA Constructor">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="FA Constructor">
+
+<link rel="icon" href="<?= ADMIN_BASE_URL ?>/images/favicon.png" type="image/x-icon"/>
+
+<?php
+$version = time();
+echo '<link rel="stylesheet" type="text/css" href="' . ADMIN_BASE_URL . '/styles/style.min.css?v' . $version . '">';
+?>
